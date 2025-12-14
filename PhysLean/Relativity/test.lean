1c952a7757f69cc64dc6f217ed673147dcfb2869
DHTT <prparadoxes@gmail.com>
2025-12-14 02:12:41 +0100

    .

--- PhysLean/Relativity/test.lean
+++ PhysLean/Relativity/test.lean
@@ -1,12 +1,12 @@
 import Mathlib.Geometry.Manifold.Algebra.LieGroup
 import PhysLean.Relativity.MinkowskiMatrix
+import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
+import Mathlib.Analysis.Matrix.Normed
 
-open minkowskiMatrix
-open Matrix
+open minkowskiMatrix Matrix GeneralLinearGroup
 
-variable (d)
 
-def LorentzGroup : Subgroup (GL (Fin 1 ⊕ Fin d) ℝ) where
+def LorentzGroup (d) : Subgroup (GL (Fin 1 ⊕ Fin d) ℝ) where
   carrier := {Λ | (dual Λ.val) * Λ = 1}
   one_mem' := by simp
   mul_mem' := by
@@ -20,9 +20,40 @@ def LorentzGroup : Subgroup (GL (Fin 1 ⊕ Fin d) ℝ) where
       simp only [coe_units_inv, Matrix.mul_inv_rev, inv_inv_of_invertible, inv_one]
       rw [←h, dual_dual]
       simp
-    . simp [Matrix.GeneralLinearGroup.det_ne_zero a]
+    . simp [det_ne_zero a]
 
-#synth (TopologicalSpace (LorentzGroup d))
-#synth (IsTopologicalGroup (LorentzGroup d))
-#synth (∀ (a : LorentzGroup d), Invertible a.val.val)
-#synth (Group (LorentzGroup d))
+
+namespace LorentzGroup
+
+variable {d}
+
+scoped[LorentzGroup] notation (name := lorentzGroup_notation) "𝓛" => LorentzGroup
+
+instance : Coe (𝓛 d) (Matrix (Fin 1 ⊕ Fin d) (Fin 1 ⊕ Fin d) ℝ) where
+  coe a := a.val.val
+
+variable (Λ : 𝓛 d)
+
+abbrev toMatrix := Λ.val.val
+
+scoped[LorentzGroup] postfix:max "ᵐ" => LorentzGroup.toMatrix
+
+lemma det_ne_zero : Λᵐ.det ≠ 0 := GeneralLinearGroup.det_ne_zero Λ.val
+
+noncomputable def transpose : 𝓛 d :=
+  ⟨GeneralLinearGroup.mkOfDetNeZero Λᵐᵀ (by simp [det_ne_zero]), sorry⟩
+
+open Manifold
+open scoped Manifold
+
+open scoped Matrix.Norms.Operator
+
+open scoped Manifold
+
+
+
+
+instance (n : Type*) [Fintype n] [DecidableEq n] :
+    LieGroup 𝓘(ℝ, Matrix n n ℝ) ⊤
+      (GeneralLinearGroup n ℝ) := by
+infer_instance
